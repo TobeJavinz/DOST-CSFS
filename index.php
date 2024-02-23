da87bcae39705f7128a060d4d3eac9316820623f
Your Name <deandecierdo@gmail.com>
2024-02-23 14:08:24 +0800

    .

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="loginstyle.css">
-    <link rel="icon" href="fav.ico" type="image/x-icon">
+    <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
     <title>DOST- CSFS</title>
 </head>
 
