f5fb1867fdb4e7238267b8fa7554b333c4699bbd
Your Name <deandecierdo@gmail.com>
2024-02-28 10:48:54 +0800

    .

--- dashboard.php
+++ dashboard.php
@@ -12,6 +12,8 @@ $name = $_SESSION['name'];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./src/output.css" rel="stylesheet">
+  <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+  <title>DOST-CSFS</title>
 </head>
 
 <body>
