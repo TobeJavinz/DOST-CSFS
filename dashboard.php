7992f2f0e8de4616b8a99a83ea77c3992b9d1f52
Rod Olinor <145325713+duotrods@users.noreply.github.com>
2024-03-18 13:28:05 +0800

    march 18

--- dashboard.php
+++ dashboard.php
@@ -10,38 +10,7 @@
 
 <body class="text-gray-800 font-inter">
   <!-- start: Sidebar -->
-  <div class="fixed left-0 top-0 w-64 h-full bg-gray-900 p-4 z-50 sidebar-menu transition-transform">
-    <a href="#" class="flex items-center pb-4 border-b border-b-gray-800">
-      <img src="./assets/1.png" alt="" class="w-8 h-8 rounded object-cover" />
-      <span class="text-lg font-bold text-white ml-3">DOST CSFS</span>
-    </a>
-    <ul class="mt-4">
-      <li class="mb-1 group active">
-        <a href="#"
-          class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-          <i class="ri-home-2-line mr-3 text-lg"></i>
-          <span class="text-sm">Dashboard</span>
-        </a>
-      </li>
-
-      <li class="mb-1 group">
-        <a href="forms.php"
-          class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-          <i class="ri-survey-line mr-3 text-lg"></i>
-          <span class="text-sm">Forms</span>
-        </a>
-      </li>
-
-      <li class="mb-1 group">
-        <a href="#"
-          class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-          <i class="ri-settings-2-line mr-3 text-lg"></i>
-          <span class="text-sm">Reports</span>
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div class="fixed top-0 left-0 w-full h-full bg-black/50 z-40 md:hidden sidebar-overlay"></div>
+  <?php include 'sidebar.php' ?>
   <!-- end: Sidebar -->
 
   <!-- start: Main -->
