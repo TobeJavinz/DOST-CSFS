9fe1d99cf45613325e3e8351a9f600cde273be88
Rod Olinor <145325713+duotrods@users.noreply.github.com>
2024-04-03 14:12:15 +0800

    semi smi final

--- src/script.js
+++ src/script.js
@@ -1,3 +1,43 @@
+// start: Sidebar
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const sidebarOverlay = document.querySelector(".sidebar-overlay");
+const sidebarMenu = document.querySelector(".sidebar-menu");
+const main = document.querySelector(".main");
+if (window.innerWidth < 768) {
+  main.classList.toggle("active");
+  sidebarOverlay.classList.toggle("hidden");
+  sidebarMenu.classList.toggle("-translate-x-full");
+}
+sidebarToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  main.classList.toggle("active");
+  sidebarOverlay.classList.toggle("hidden");
+  sidebarMenu.classList.toggle("-translate-x-full");
+});
+sidebarOverlay.addEventListener("click", function (e) {
+  e.preventDefault();
+  main.classList.add("active");
+  sidebarOverlay.classList.add("hidden");
+  sidebarMenu.classList.add("-translate-x-full");
+});
+document.querySelectorAll(".sidebar-dropdown-toggle").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    const parent = item.closest(".group");
+    if (parent.classList.contains("selected")) {
+      parent.classList.remove("selected");
+    } else {
+      document
+        .querySelectorAll(".sidebar-dropdown-toggle")
+        .forEach(function (i) {
+          i.closest(".group").classList.remove("selected");
+        });
+      parent.classList.add("selected");
+    }
+  });
+});
+// end: Sidebar
+
 // start: Popper
 const popperInstance = {};
 document.querySelectorAll(".dropdown").forEach(function (item, index) {
@@ -72,25 +112,25 @@ function hidePopper(popperId) {
 }
 // end: Popper
 
-// const groups = document.querySelectorAll(".group");
-// groups.forEach((groups) => {
-//   groups.addEventListener("click", () => {
-//     groups.classList.add("active");
-//   });
-// });
+const groups = document.querySelectorAll(".group");
+groups.forEach((groups) => {
+  groups.addEventListener("click", () => {
+    groups.classList.add("active");
+  });
+});
 
-// const showPasswordCheckbox = document.getElementById("showPassword");
-// const passwordField = document.getElementById("password");
+const showPasswordCheckbox = document.getElementById("showPassword");
+const passwordField = document.getElementById("password");
 
-// showPasswordCheckbox.addEventListener("change", function () {
-//   const type = showPasswordCheckbox.checked ? "text" : "password";
-//   passwordField.setAttribute("type", type);
-// });
+showPasswordCheckbox.addEventListener("change", function () {
+  const type = showPasswordCheckbox.checked ? "text" : "password";
+  passwordField.setAttribute("type", type);
+});
 
-// const logshowPasswordCheckbox = document.getElementById("logshowPassword");
-// const logpasswordField = document.getElementById("logpassword");
+const logshowPasswordCheckbox = document.getElementById("logshowPassword");
+const logpasswordField = document.getElementById("logpassword");
 
-// logshowPasswordCheckbox.addEventListener("change", function () {
-//   const type = logshowPasswordCheckbox.checked ? "text" : "password";
-//   logpasswordField.setAttribute("type", type);
-// });
+logshowPasswordCheckbox.addEventListener("change", function () {
+  const type = logshowPasswordCheckbox.checked ? "text" : "password";
+  logpasswordField.setAttribute("type", type);
+});
