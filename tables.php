c6b91b1f043209458e006d2ccf7f1d81caae77a9
Your Name <deandecierdo@gmail.com>
2024-04-03 09:51:28 +0800

    Remove empty lines in tables.php

--- tables.php
+++ tables.php
@@ -30,8 +30,6 @@ $result = mysqli_query($conn, $query);
   <?php include 'sidebar.php' ?>
   <!-- end: Sidebar -->
 
-  
-
   <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
     <div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
       <button type="button" class="text-lg text-gray-600 sidebar-toggle">
